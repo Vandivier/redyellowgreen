01e5b9c8fbc1060185a5ca99962cfc76d3240219
Vandivier <john@afterecon.com>
2023-05-17 21:18:56 -0400

    feat: unique social url
    per platform
    insta, linkedin, twitter, tiktok

--- db/migrations/0_init/migration.sql
+++ db/migrations/0_init/migration.sql
@@ -41,6 +41,24 @@ CREATE TABLE "Token" (
     CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "SocialMediaContent" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "tikTokUrl" TEXT NOT NULL,
+    "youTubeUrl" TEXT NOT NULL,
+    "instagramUrl" TEXT NOT NULL,
+    "twitterUrl" TEXT NOT NULL,
+    "linkedInUrl" TEXT NOT NULL,
+    "publishedAt" TIMESTAMP(3) NOT NULL,
+    "description" TEXT NOT NULL,
+    "currentPage" INTEGER NOT NULL,
+    "scrapedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "SocialMediaContent_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
@@ -50,8 +68,24 @@ CREATE UNIQUE INDEX "Session_handle_key" ON "Session"("handle");
 -- CreateIndex
 CREATE UNIQUE INDEX "Token_hashedToken_type_key" ON "Token"("hashedToken", "type");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "SocialMediaContent_tikTokUrl_key" ON "SocialMediaContent"("tikTokUrl");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SocialMediaContent_youTubeUrl_key" ON "SocialMediaContent"("youTubeUrl");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SocialMediaContent_instagramUrl_key" ON "SocialMediaContent"("instagramUrl");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SocialMediaContent_twitterUrl_key" ON "SocialMediaContent"("twitterUrl");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SocialMediaContent_linkedInUrl_key" ON "SocialMediaContent"("linkedInUrl");
+
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
